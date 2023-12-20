f24cad4ad6683d090162b42d87251a471daaa76b
B1229049 <B1229049@cgu.edu.tw>
2023-12-20 21:30:35 +0800

    Hw5(2)

--- Hw5/main2.c
+++ Hw5/main2.c
@@ -1,18 +1,32 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
 #include <stdio.h>
 
-int main(void) {
-  double pi = 4.0f;
-  int n = 0;
-  for (int i=2; i<=200000; i++){
-    if (i%2==0) {
-      n = -1;
+int main()
+{
+    double pi = 4.0f;
+    int n = 1;
+    double x = 0;
+    for (int i=2; i <= 200000; i++){
+        if (i % 2 == 0){
+            n = -1;
+        }
+        else {
+            n = 1;
+        }
+        x = n*(4.0f/((2.0f*i)-1));
+        pi = pi + x;
+        if (pi < 3.141594 && pi > 3.141585){
+            printf("%f\n",x);
+            break;
+        }
     }
-    else {
-      n = 1;
-    }
-    pi = pi + n*(4.0f/((2.0f*i)-1));
-  }
-  printf("%.5lf",pi);
-
-  return 0;
+    
+    return 0;
 }
